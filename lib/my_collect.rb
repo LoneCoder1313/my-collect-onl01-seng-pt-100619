e6f8abe8e82a8e4765afdca81244e133dd16919f
Ricky dildy <rdildy92@gmail.com>
2019-10-29 23:56:51 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,5 +2,9 @@ my_collect(array)
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 my_collect(array) do |name|
   name.split("").first 
-end 
+end
 
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase 
+end 
